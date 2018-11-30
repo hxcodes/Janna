9c6bb2824c9e701fd9ee07ddaf0df73153914175
暮雨輕雲 <publiction@163.com>
2018-11-30 16:15:50 +0800

    upload codes first time

---
+++ src/main/java/com/hxcodes/janna/JannaApplication.java
@@ -0,0 +1,12 @@
+package com.hxcodes.janna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JannaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JannaApplication.class, args);
+	}
+}
